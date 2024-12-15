3bf28f15db6c35d419913b41302ee5f10416836a
Bruno Lima <bruno.lima@grupotel.com.br>
2024-12-15 15:15:31 -0300

    Adicionado diretivas com interpolações no front

--- front/eventos/src/app/eventos/eventos.component.ts
+++ front/eventos/src/app/eventos/eventos.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrl: './eventos.component.scss'
 })
-export class EventosComponent {
+export class EventosComponent implements OnInit {
 
+  // PRorpiedades
+  eventos: any = []
+
+  // Ijetores
+  constructor() {}
+
+  // Inicialização
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  // Métodos
+  getEventos(): void{
+
+    this.eventos = [
+      {
+        Tema: 'Angular',
+        Local: 'Belo Horizonte'
+      },
+      {
+        Tema: 'React',
+        Local: 'São Paulo'
+      },
+      {
+        Tema: 'Vue',
+        Local: 'Rio de Janeiro'
+      }
+    ]
+  }
 }
+
