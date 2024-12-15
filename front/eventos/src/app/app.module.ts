00d986f7bb7a9e0fe32149b322841433bca21296
Bruno Lima <bruno.lima@grupotel.com.br>
2024-12-15 13:50:53 -0300

    componentes palestrantes e eventos

--- front/eventos/src/app/app.module.ts
+++ front/eventos/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    EventosComponent,
+      PalestrantesComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
